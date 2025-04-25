12fa1e107b7963ba017f4aadb8a33706faaa88ed
Alexandre Barret <barret.alx@gmail.com>
2025-04-25 22:08:06 +1200

    Test TB2.clear

--- test/termbox2_test.rb
+++ test/termbox2_test.rb
@@ -34,10 +34,32 @@ class TestTermbox2 < Minitest::Test
     end
   end
 
+  def test_tb_clear
+    Shell.run(command: command("tb_clear.rb"), height: 5) do |shell|
+      shell.send_keys('r')
+      assert_equal "{type: 1, key: 0, ch: 114, mod: 0, x: 0, y: 0, w: 0, h: 0}\n\n\n\n\n", shell.screenshot
+
+      shell.send_keys('c') # triggers TB2.clear
+
+      assert_equal "\n\n\n\n\n", shell.screenshot
+      shell.send_keys('q')
+    end
+  end
+
+  private
+
   def config_for(name)
     [
-      "bin/run ./test/examples/#{name}.rb",
-      File.read("test/examples/#{name}.ansi")
+      command("#{name}.rb"),
+      File.read(example("#{name}.ansi"))
     ]
+  end
+
+  def command(name)
+    "bin/run #{example(name)}"
+  end
+
+  def example(name)
+    File.join(Dir.pwd, "test", "examples", name)
   end
 end
