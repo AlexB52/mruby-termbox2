4229296ea79a4eb3041abe9be9d9350aa370079b
Alexandre Barret <barret.alx@gmail.com>
2025-04-25 21:27:35 +1200

    Refactor test setup

--- test/termbox2_test.rb
+++ test/termbox2_test.rb
@@ -2,27 +2,34 @@ require 'test_helper'
 
 class TestTermbox2 < Minitest::Test
   def test_tb_print
-    command = "bin/run ./test/examples/tb_print_hello_world.rb"
+    command, expectation = config_for("tb_print_hello_world")
     Shell.run(command: command) do |shell|
-      assert_equal File.read("test/examples/tb_print_hello_world.ansi"), shell.screenshot
+      assert_equal expectation, shell.screenshot
       shell.send_key('q')
     end
   end
 
   def test_tb_color
-    command = "bin/run ./test/examples/tb_print_color.rb"
+    command, expectation = config_for("tb_print_color")
     Shell.run(command: command) do |shell|
-      assert_equal File.read("test/examples/tb_print_color.ansi"), shell.screenshot
+      assert_equal expectation, shell.screenshot
       shell.send_key('q')
     end
   end
 
   def test_tb_poll_event
-    command = "bin/run ./test/examples/tb_poll_event.rb"
+    command, expectation = config_for("tb_poll_event")
     Shell.run(command: command) do |shell|
       shell.send_key('r')
-      assert_equal File.read("test/examples/tb_poll_event.ansi"), shell.screenshot
+      assert_equal expectation, shell.screenshot
       shell.send_key('q')
     end
+  end
+
+  def config_for(name)
+    [
+      "bin/run ./test/examples/#{name}.rb",
+      File.read("test/examples/#{name}.ansi")
+    ]
   end
 end
