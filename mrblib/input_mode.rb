389da938873399d66dd417eaef0a17c7f43ccdb6
Alexandre Barret <barret.alx@gmail.com>
2025-06-23 22:12:40 +1200

    Create first iteration of the TB2 event

--- mrblib/input_mode.rb
+++ mrblib/input_mode.rb
@@ -10,4 +10,79 @@ module Termbox2
     mode = args.reduce(0) { |mask, argument| mask | FLAG_MAP[argument] }
     self.set_native_input_mode(mode)
   end
+
+  class Event
+    TYPE_KEY = 1
+    TYPE_RESIZE = 2
+    TYPE_MOUSE = 3
+
+    KEYS = {
+      CHAR: 0,
+      ESCAPE: 27,
+      DELETE: 127,
+      ENTER: 13
+    }
+
+    attr_reader :event
+
+    def initialize(event)
+      @event = event
+    end
+
+    def [](value)
+      event[value]
+    end
+
+    def inspect
+      event.inspect
+    end
+
+    def present?
+      !empty?
+    end
+
+    def empty?
+      event.nil? || event.empty?
+    end
+
+    def type
+      case event[:type]
+      when TYPE_KEY    then :key
+      when TYPE_RESIZE then :resize
+      when TYPE_MOUSE  then :mouse
+      end
+    end
+
+    def key
+      KEYS.invert[event[:key]]
+    end
+
+    def char
+      return unless key?
+      event[:ch].chr
+    end
+
+    def key?(value = nil)
+      return false unless event && event[:type] == TYPE_KEY
+
+      if value.nil?
+        return true
+      end
+
+      case value
+      when :ESCAPE
+        event[:key] == KEYS[:ESCAPE]
+      else
+        char == value
+      end
+    end
+
+    def resize?
+      event && event[:type] == TYPE_RESIZE
+    end
+
+    def mouse?
+      event && event[:type] == TYPE_MOUSE
+    end
+  end
 end
